2434ee15ea75c1c9378164bf5a10e70757c88744
Heinz <aurelianob808@gmail.com>
2024-09-29 13:10:49 +0800

    update file structure

--- src/App.vue
+++ src/App.vue
@@ -4,56 +4,34 @@ import Header from './components/Header.vue'
 </script>
 
 <template>
-  <header>
-    <!-- <img alt="Vue logo" class="logo" src="./assets/F1.png" width="125" height="125" /> -->
+  <div class="container">
     <div class="top">
       <Header />
     </div>
-  </header>
-  <body>
     <div class="search">
       <MainView/>
     </div>
-  </body>
-
+  </div>
 </template>
 
 <style scoped>
-body {
+.container {
   display: flex;
-  height: 100%;
-  width: 100%;
+  flex-direction: column;
 }
 
-header {
+.search {
+  position: absolute;
+  top: 70px;
+  left: 0px;
+  right: 0px;
+}
+
+.top {
   line-height: 1.5;
   position: absolute;
   top: 0px;
   left: 0px;
   right: 0px;
-  z-index: 1;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
 }
 </style>
--- src/pages/MainView.vue
+++ src/pages/MainView.vue
@@ -1,101 +1,108 @@
-<script setup>
-import Background from '../components/Background.vue'
-</script>
-
 <template>
-  <div class="background">
-    <Background />
-  </div>
-  <div class="search">
-    <input class="input"
-      v-model="input"
-      placeholder="请输入车队名称"
-      @focus="getInput"
-      @blur="leaveInput"
-      clearable
-    ></input>
+  <div class="container">
+    <div class="inputs">
+      <input class="input"
+        v-model="input"
+        placeholder="请输入车队名称"
+        @focus="getInput"
+        @blur="leaveInput"
+        clearable
+      ></input>
+    </div>
     <div class="list" v-show="isShow" @click="getInput">
-      <ul class="hot-teams">
-        <li
-        v-for="item in filteredData"
-        :key="item"
-        @click="handleClick(item)"
-        >
-        {{ item }}
+      <ul class="fleet">
+        <li v-for="item in filteredData" :key="item" @click="handleClick(item)">
+          {{ item }}
         </li>
       </ul>
     </div>
-    <button @click="increment">查询</button>
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      count: 0,
-      input: "",
-      isShow: "",
-      teamData: [],
-      maxFleets: 5,
-      clickedItem: "",
-    };
-  },
-  methods: {
-    increment() {
-      this.count++
-    },
-    getInput() {
-      this.isShow = true;
-    },
-    leaveInput() {
-      this.isShow = false;
-      console.log("blur");
-    },
-  },
-  mounted() {
-    this.increment()
-  }
+<script setup>
+import { ref, computed, onMounted } from 'vue';
+import axios from 'axios';
+
+const input = ref("");
+const isShow = ref(false);
+const teamData = ref([]);
+const maxTeams = 20;
+const clickedItem = ref("");
+
+const search = () => {
+  // 跳转到结果页面
+  console.log(input.value);
 };
+
+const getInput = () => {
+  isShow.value = true;
+};
+
+const leaveInput = () => {
+  isShow.value = false;
+  console.log("leave");
+};
+
+const handleClick = (item) => {
+  clickedItem.value = item;
+  input.value = item;
+  console.log(input.value);
+  search();
+};
+
+const filteredData = computed(() => {
+  return teamData.value
+    .filter((item) => item.includes(input.value))
+    .slice(0, maxTeams);
+});
+
+onMounted(async () => {
+  const options = {
+    method: "GET",
+    url: "https://api.jolpi.ca/ergast/f1/2024/constructors",
+  };
+
+  try {
+    const response = await axios.request(options);
+    const constructors = response.data.MRData.ConstructorTable.Constructors;
+    teamData.value = constructors.map(item => item.name);
+  } catch (error) {
+    console.error(error);
+  }
+});
 </script>
 
-<style>
-.background {
-  height: 100vh;
-  width: 100vw;
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: -10;
-}
-
-.search {
-  z-index: 0;
-  display: flex;
+<style scoped>
+.container {
+  background-image: url("../assets/background.avif");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
   justify-content: center;
   align-items: center;
-  .input {
-    border-radius: 5px;
-    padding: 10px;
-    outline: none;
-    display: flex;
-    font-size: 18px;
-    width: 300px;
-    height: 40px;
-  }
-  button {
-    border: none;
-    cursor: pointer;
-    color: white;
-    background-color: #dc112c;
-    border-radius: 5px;
-    font-size: 18px;
-    min-width: 60px;
-    min-height: 40px;
-  }
-  button:hover {
-    background-color: #38043c;
-  }
+  height: calc(100vh - 70px);
+  width: 100vw;
+  margin: 0 auto;
 }
 
+.input {
+  margin: 0 auto;
+  border-radius: 5px;
+  padding: 10px;
+  outline: none;
+  font-size: 18px;
+  width: 300px;
+  height: 40px;
+  display: flex;
+}
+
+.list {
+    background-color: white;
+    width: 300px;
+    height: 300px;
+    margin: 0 auto;
+    border-radius: 5px;
+    box-sizing: border-box;
+    padding-top: 20px;
+  }
 </style>
