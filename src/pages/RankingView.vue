88ab0bca290015635473e345c62437312bc191b8
Heinz <aurelianob808@gmail.com>
2024-10-09 14:35:36 +0800

    update rank list

--- src/pages/RankingView.vue
+++ src/pages/RankingView.vue
@@ -4,7 +4,11 @@
       <Header />
     </div>
     <div class="body">
-      <DriverRanking />
+      <div class="buttons">
+        <button class="my-button" @click="currentComponent = 'DriverRanking'">车手排名</button>
+        <button class="my-button" @click="currentComponent = 'TeamRanking'">车队排名</button>
+      </div>
+      <component :is="currentComponent"></component>
     </div>
   </div>
 </template>
@@ -15,11 +19,18 @@ import { useRoute, useRouter } from 'vue-router';
 import axios from 'axios';
 import Header from '../components/Header.vue';
 import DriverRanking from '../components/DriverRanking.vue';
+import TeamRanking from '../components/TeamRanking.vue';
 
 export default {
   components: {
     Header,
     DriverRanking,
+    TeamRanking,
+  },
+  data() {
+    return {
+      currentComponent: 'DriverRanking'
+    };
   }
 }
 </script>
@@ -33,8 +44,15 @@ export default {
   right: 0px;
 }
 
-.body{
+.buttons {
   display: flex;
+  justify-content: center;
+  gap: 10px;
+}
+
+.body {
+  display: flex;
+  flex-direction: column;
   justify-content: center;
   height: calc(100vh - 70px);
   width: 100vw;
@@ -42,5 +60,32 @@ export default {
   top: 70px;
   left: 0px;
   right: 0px;
+}
+
+.my-button {
+  background-color: #eaeff1;
+  color: rgb(35, 32, 32);
+  padding: 10px 20px;
+  border: 2px solid #7d7e7f;
+  border-radius: 25px;
+  font-size: 16px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+}
+
+.my-button:disabled {
+  background-color: #ccc;
+  cursor: not-allowed;
+}
+
+.my-button:hover {
+  border-color: #003d80;
+}
+
+.my-button:active,
+.my-button:focus {
+  background-color: #478fee;
+  color: rgb(255, 255, 255);
+  outline: none;
 }
 </style>
