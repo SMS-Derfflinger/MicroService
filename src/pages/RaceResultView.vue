63d3cf236eef15aff4661fa45619d29c06c46167
Heinz <aurelianob808@gmail.com>
2024-10-11 20:47:46 +0800

    update race result view

--- src/pages/RaceResultView.vue
+++ src/pages/RaceResultView.vue
@@ -13,8 +13,8 @@
 </template>
 
 <script>
-import Header from '../components/Header.vue';
-import RaceIntro from '../components/RaceIntro.vue';
+import Header from '@/components/Header.vue';
+import RaceIntro from '@/components/RaceIntro.vue';
 import CircuitInfo from '@/components/CircuitInfo.vue';
 import RaceInfo from '@/components/RaceInfo.vue';
 import RaceResult from '@/components/RaceResult.vue';
@@ -29,3 +29,23 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.top {
+  line-height: 1.5;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+}
+
+.body {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 50vw;
+  position: absolute;
+  top: 70px;
+  left: 25vw;
+}
+</style>
